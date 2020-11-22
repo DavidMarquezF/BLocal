70d21f3b0992a96fb23260478e0afecd90865866
unknown <aveho002@edu.xamk.fi>
2020-11-22 11:42:35 +0100

    Repository items CRUD methods

--- BLocalApp/app/src/main/java/com/bteam/blocal/model/Repository.java
+++ BLocalApp/app/src/main/java/com/bteam/blocal/model/Repository.java
@@ -7,9 +7,12 @@ import androidx.annotation.NonNull;
 import androidx.annotation.Nullable;
 import androidx.lifecycle.MutableLiveData;
 
+import com.google.android.gms.tasks.OnCompleteListener;
 import com.google.android.gms.tasks.OnFailureListener;
 import com.google.android.gms.tasks.OnSuccessListener;
+import com.google.android.gms.tasks.Task;
 import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.firestore.CollectionReference;
 import com.google.firebase.firestore.DocumentReference;
 import com.google.firebase.firestore.DocumentSnapshot;
 import com.google.firebase.firestore.EventListener;
@@ -18,18 +21,17 @@ import com.google.firebase.firestore.FirebaseFirestoreException;
 import com.google.firebase.firestore.QuerySnapshot;
 
 import java.util.ArrayList;
-import java.util.Random;
-import java.util.concurrent.ExecutorService;
-import java.util.concurrent.Executors;
 
 public class Repository {
     private static final String TAG = "REPOSITORY";
     private FirebaseAuth auth;
-
     private FirebaseFirestore db;
-    private ExecutorService executor;
-
+    private CollectionReference itemCollection = db.collection("items");
+    //Persisted items
     private MutableLiveData<ArrayList<ItemModel>> items;
+    private MutableLiveData<DocumentSnapshot> selectedItem;
+    //Search functionality
+    Boolean found = false;
 
     //Auth - is this even needed?
     public FirebaseAuth auth(){
@@ -39,12 +41,14 @@ public class Repository {
     // Repository methods
     public Repository() {
         db = FirebaseFirestore.getInstance();
-        executor = Executors.newSingleThreadExecutor();
+        if(itemCollection == null){
+            itemCollection = db.collection("items");
+        }
         //Items initialisation
         if(items==null){
             items = new MutableLiveData<ArrayList<ItemModel>>();
         }
-        db.collection("items")
+        itemCollection
                 .addSnapshotListener(new EventListener<QuerySnapshot>() {
                     @Override
                     public void onEvent(@Nullable QuerySnapshot snapshot, @Nullable FirebaseFirestoreException error) {
@@ -65,7 +69,8 @@ public class Repository {
     //Database CRUD methods
     //CREATE
     public void addItem(ItemModel item){
-        db.collection("items").add(new ItemModel(item.getUid(), item.getName(), item.getImageUrl(), item.getPrice(), item.getStock()))
+        itemCollection
+                .add(new ItemModel(item.getUid(), item.getName(), item.getImageUrl(), item.getPrice(), item.getStock()))
                 .addOnSuccessListener(new OnSuccessListener<DocumentReference>() {
                     @Override
                     public void onSuccess(DocumentReference documentReference) {
@@ -75,14 +80,14 @@ public class Repository {
                 .addOnFailureListener(new OnFailureListener() {
                     @Override
                     public void onFailure(@NonNull Exception e) {
-                        Log.d(TAG, "Adding item failed: ", e);
+                        Log.e(TAG, "Adding item failed: ", e);
                     }
                 });
     }
 
     //READ
     public MutableLiveData<ArrayList<ItemModel>> getItems(){
-        db.collection("items")
+        itemCollection
                 .addSnapshotListener(new EventListener<QuerySnapshot>() {
                     @Override
                     public void onEvent(@Nullable QuerySnapshot snapshot, @Nullable FirebaseFirestoreException error) {
@@ -101,17 +106,68 @@ public class Repository {
         return items;
     }
 
+    public MutableLiveData<DocumentSnapshot> getItem(String uid){
+        found = false;
+        itemCollection.document(uid).get()
+                .addOnCompleteListener(new OnCompleteListener<DocumentSnapshot>() {
+                    @Override
+                    public void onComplete(@NonNull Task<DocumentSnapshot> task) {
+                        if (task.isSuccessful()) {
+                            DocumentSnapshot document = task.getResult();
+                            if (document.exists()) {
+                                Log.d(TAG, "DocumentSnapshot data: " + document.getData());
+                                if(selectedItem == null){
+                                    selectedItem = new MutableLiveData<DocumentSnapshot>();
+                                }
+                                selectedItem.setValue(document);
+                                found = true;
+                            } else {
+                                Log.d(TAG, "No such document");
+                            }
+                        } else {
+                            Log.d(TAG, "get failed with ", task.getException());
+                        }
+                    }
+
+                });
+        if(found){
+            return selectedItem;
+        } else {
+            return null;
+        }
+    }
+
     //UPDATE
+    //Note that if a document with corresponding name isn't found, it will be created
+    public void updateItem(ItemModel item){
+        itemCollection.document(item.getUid()).set(item);
+    }
 
     //DELETE
     public void deleteAll(){
-        db.collection("items").get()
+        itemCollection.get()
                 .addOnSuccessListener(new OnSuccessListener<QuerySnapshot>() {
                     @Override
                     public void onSuccess(QuerySnapshot snapshots) {
                         for(DocumentSnapshot doc : snapshots){
                             doc.getReference().delete();
                         }
+                    }
+                });
+    }
+
+    public void deleteItem(ItemModel item){
+        itemCollection.document(item.getUid()).delete()
+                .addOnSuccessListener(new OnSuccessListener<Void>() {
+                    @Override
+                    public void onSuccess(Void aVoid) {
+                        Log.d(TAG, "Deleted item " + item.getName());
+                    }
+                })
+                .addOnFailureListener(new OnFailureListener() {
+                    @Override
+                    public void onFailure(@NonNull Exception e) {
+                        Log.e(TAG, "Error deleting document: " + e);
                     }
                 });
     }
