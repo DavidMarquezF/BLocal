2ac57e747ff881c82d53f524c7e1da70ec0d4b5e
Ondrej Viskup <viskup@pm.me>
2020-11-26 18:18:01 +0100

    MainUser: refactoring layouts

--- BLocalApp/app/src/main/java/com/bteam/blocal/ui/map/MapsFragment.java
+++ BLocalApp/app/src/main/java/com/bteam/blocal/ui/map/MapsFragment.java
@@ -2,22 +2,54 @@ package com.bteam.blocal.ui.map;
 
 import androidx.annotation.NonNull;
 import androidx.annotation.Nullable;
+import androidx.core.app.ActivityCompat;
 import androidx.fragment.app.Fragment;
+import androidx.lifecycle.Observer;
+import androidx.lifecycle.ViewModelProvider;
 
+import android.Manifest;
+import android.annotation.SuppressLint;
+import android.content.pm.PackageManager;
+import android.location.Location;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 
 import com.bteam.blocal.R;
+import com.bteam.blocal.model.StoreModel;
+import com.google.android.gms.location.FusedLocationProviderClient;
+import com.google.android.gms.location.LocationAvailability;
+import com.google.android.gms.location.LocationCallback;
+import com.google.android.gms.location.LocationRequest;
+import com.google.android.gms.location.LocationResult;
+import com.google.android.gms.location.LocationServices;
 import com.google.android.gms.maps.CameraUpdateFactory;
 import com.google.android.gms.maps.GoogleMap;
 import com.google.android.gms.maps.OnMapReadyCallback;
 import com.google.android.gms.maps.SupportMapFragment;
 import com.google.android.gms.maps.model.LatLng;
+import com.google.android.gms.maps.model.Marker;
 import com.google.android.gms.maps.model.MarkerOptions;
+import com.google.android.gms.tasks.CancellationToken;
+import com.google.android.gms.tasks.OnFailureListener;
+import com.google.android.gms.tasks.OnSuccessListener;
+import com.google.android.gms.tasks.OnTokenCanceledListener;
+
+import java.util.ArrayList;
+import java.util.List;
+
+import static com.google.android.gms.location.LocationRequest.PRIORITY_HIGH_ACCURACY;
 
 public class MapsFragment extends Fragment {
+    private static final String TAG = "MapsFragment";
+    private static final int LOCATION_PERMISSION_REQUEST_CODE = 1111;
+
+    private GoogleMap gMap;
+    private Marker currentLocationMarker;
+    private List<Marker> storeMarkers = new ArrayList<>();
+    private MapsViewModel mapsViewModel;
 
     private OnMapReadyCallback callback = new OnMapReadyCallback() {
 
@@ -32,9 +64,10 @@ public class MapsFragment extends Fragment {
          */
         @Override
         public void onMapReady(GoogleMap googleMap) {
-            LatLng sydney = new LatLng(-34, 151);
-            googleMap.addMarker(new MarkerOptions().position(sydney).title("Marker in Sydney"));
-            googleMap.moveCamera(CameraUpdateFactory.newLatLng(sydney));
+            gMap = googleMap;
+//            LatLng sydney = new LatLng(-34, 151);
+//            googleMap.addMarker(new MarkerOptions().position(sydney).title("Marker in Sydney"));
+//            googleMap.moveCamera(CameraUpdateFactory.newLatLng(sydney));
         }
     };
 
@@ -49,10 +82,49 @@ public class MapsFragment extends Fragment {
     @Override
     public void onViewCreated(@NonNull View view, @Nullable Bundle savedInstanceState) {
         super.onViewCreated(view, savedInstanceState);
+
+        mapsViewModel = new ViewModelProvider(this).get(MapsViewModel.class);
+
+        checkLocationPermissions();
+
         SupportMapFragment mapFragment =
                 (SupportMapFragment) getChildFragmentManager().findFragmentById(R.id.map);
         if (mapFragment != null) {
             mapFragment.getMapAsync(callback);
+        }
+
+        // Show the current location on the map
+        mapsViewModel.getCurrentLocation().observe(getViewLifecycleOwner(), location -> {
+            if (null != currentLocationMarker) currentLocationMarker.remove();
+            LatLng current = new LatLng(location.getLatitude(), location.getLongitude());
+            currentLocationMarker = gMap.addMarker(new MarkerOptions().position(current)
+                    .title("You are here!"));
+            gMap.moveCamera(CameraUpdateFactory.newLatLngZoom(current, 15));
+        });
+    }
+
+
+    private void checkLocationPermissions() {
+        if (ActivityCompat.checkSelfPermission(getContext(),
+                Manifest.permission.ACCESS_FINE_LOCATION) != PackageManager.PERMISSION_GRANTED) {
+            requestPermissions(new String[]{Manifest.permission.ACCESS_FINE_LOCATION},
+                    LOCATION_PERMISSION_REQUEST_CODE);
+        } else {
+            Log.d(TAG, "checkLocationPermissions: Location permission already granted.");
+        }
+    }
+
+    @Override
+    public void onRequestPermissionsResult(int requestCode, @NonNull String[] permissions,
+                                           @NonNull int[] grantResults) {
+        super.onRequestPermissionsResult(requestCode, permissions, grantResults);
+
+        if (requestCode == LOCATION_PERMISSION_REQUEST_CODE &&
+                permissions[0].equals(Manifest.permission.ACCESS_FINE_LOCATION) &&
+                grantResults[0] == PackageManager.PERMISSION_GRANTED) {
+            Log.d(TAG, "onRequestPermissionsResult: Location permission was successfully obtained.");
+        } else {
+            Log.d(TAG, "onRequestPermissionsResult: Location permission was not granted!");
         }
     }
 }
---
+++ BLocalApp/app/src/main/java/com/bteam/blocal/ui/map/MapsViewModel.java
@@ -0,0 +1,78 @@
+package com.bteam.blocal.ui.map;
+
+import android.annotation.SuppressLint;
+import android.app.Application;
+import android.location.Location;
+import android.util.Log;
+
+import androidx.lifecycle.AndroidViewModel;
+import androidx.lifecycle.LiveData;
+import androidx.lifecycle.MutableLiveData;
+
+import com.bteam.blocal.model.StoreModel;
+import com.google.android.gms.location.FusedLocationProviderClient;
+import com.google.android.gms.location.LocationServices;
+import com.google.android.gms.maps.GoogleMap;
+
+import java.util.List;
+
+public class MapsViewModel extends AndroidViewModel {
+    private static final String TAG = "MapsViewModel";
+
+    private MutableLiveData<List<StoreModel>> nearbyStores;
+    private MutableLiveData<StoreModel> selectedStore;
+
+    private FusedLocationProviderClient fusedLocationClient;
+    private MutableLiveData<Location> currentLocation;
+
+    public MapsViewModel(Application application) {
+        super(application);
+        this.nearbyStores = new MutableLiveData<>();
+        this.selectedStore = new MutableLiveData<>();
+        this.currentLocation = new MutableLiveData<>();
+
+        this.fusedLocationClient = LocationServices.getFusedLocationProviderClient(application);
+
+        requestCurrentLocation();
+    }
+
+    public LiveData<List<StoreModel>> getNearbyStores() {
+        return nearbyStores;
+    }
+
+    public void setNearbyStores(List<StoreModel> nearbyStores) {
+        this.nearbyStores.setValue(nearbyStores);
+    }
+
+    public LiveData<StoreModel> getSelectedStore() {
+        return selectedStore;
+    }
+
+    public void setSelectedStore(StoreModel selectedStore) {
+        this.selectedStore.setValue(selectedStore);
+    }
+
+    public LiveData<Location> getCurrentLocation() {
+        return currentLocation;
+    }
+
+    public void setCurrentLocation(Location currentLocation) {
+        this.currentLocation.setValue(currentLocation);
+    }
+
+    @SuppressLint("MissingPermission")
+    private void requestCurrentLocation() {
+        fusedLocationClient.getLastLocation()
+                .addOnSuccessListener(location -> {
+                    if (null != location) {
+                        Log.d(TAG, "requestCurrentLocation: Obtained last location!");
+                        currentLocation.setValue(location);
+                    } else {
+                        Log.d(TAG, "requestCurrentLocation: Last location was not obtained!");
+                    }
+                })
+                .addOnFailureListener(e -> Log.e(TAG, "requestCurrentLocation: " + e.getLocalizedMessage(), e)
+                );
+    }
+
+}
